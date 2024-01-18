ce16f171bde9efd3792b6589f4cf9d8f913b276d
AlexChe360 <devalex360@gmail.com>
2024-01-18 08:37:40 +0600

    Configuration project

---
+++ TomatoTimer/Sources/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  TomatoTimer
+//
+//  Created by Alex on 18.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Constants
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+    }
+    
+    // MARK: - Setups
+    
+    private func setupView() {
+        
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    // MARK: - Actions
+}
+
